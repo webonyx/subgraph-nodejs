a18a2988f418e4ad386cc2cb602bb1349e6e3e9f
Viet Pham <viet@webonyx.com>
2022-05-10 21:54:00 +0700

    --ignore-stdin

--- validate-schema.sh
+++ validate-schema.sh
@@ -4,4 +4,4 @@ set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-xh -A bearer -a "$SCHEMA_REGISTRY_TOKEN" https://schema-registry.team.us/schema/validate name=subgraph-atlas-app version=1.0 type_defs=@"${DIR}"/apps.graphql url=https://subgraph.team.us/atlas-app
+xh -A bearer -a "$SCHEMA_REGISTRY_TOKEN" https://schema-registry.team.us/schema/validate name=subgraph-atlas-app version=1.0 type_defs=@"${DIR}"/apps.graphql url=https://subgraph.team.us/atlas-app --ignore-stdin
